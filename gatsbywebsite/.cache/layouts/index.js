c92c01858483354834f6bde934dc951c3082c139
AC De Leon <aadeleon@sdsu.edu>
2018-07-24 12:10:58 -0700

    Merge branch 'dev-restartDeploy' [*] Yarn develop seems to work

---
+++ gatsbywebsite/.cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "C:/Users/EVADELEON2/Documents/acfromspace.github.io/gatsbywebsite/src/layouts/index.js"
+  import data from "C:\\Users\\EVADELEON2\\Documents\\acfromspace.github.io\\gatsbywebsite\\.cache\\json\\layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
